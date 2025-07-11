cd325e39ce42f708d504fb3972f00faa6414afa5
Zag207 <danilka.stepanov04@gmail.com>
2025-07-11 16:12:22 +0300

    Comment configuration: PK and relationships

---
+++ SmartTraсker.Data/Configurations/CommentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTraсker.Data.Models;
+
+namespace SmartTraсker.Data.Configurations;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder
+            .HasOne(c => c.Author)
+            .WithMany(u => u.Comments)
+            .HasForeignKey(c => c.AuthorId);
+        
+        builder
+            .HasOne(c => c.Task)
+            .WithMany(t => t.Comments)
+            .HasForeignKey(c => c.TaskId);
+    }
+}
