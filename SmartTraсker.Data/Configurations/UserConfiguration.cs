aae7b7caf003834f5e9865552ab7e50f5ca0812e
Zag207 <danilka.stepanov04@gmail.com>
2025-07-11 16:01:38 +0300

    User configuration: PK and relationships

---
+++ SmartTraсker.Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTraсker.Data.Models;
+
+namespace SmartTraсker.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder
+            .HasMany(u => u.CreatedTasks)
+            .WithOne(t => t.Author);
+        
+        builder
+            .HasMany(u => u.AssignedTasks)
+            .WithOne(t => t.Executor);
+        
+        builder
+            .HasMany(u => u.Comments)
+            .WithOne(c => c.Author);
+    }
+}
