06c5b87435a9e0d9a4bf50606cac8b158c228a00
Zag207 <danilka.stepanov04@gmail.com>
2025-07-11 16:09:41 +0300

    Task configuration: PK and relationships

---
+++ SmartTraсker.Data/Configurations/TaskConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTraсker.Data.Models;
+using Task = SmartTraсker.Data.Models.Task;
+
+namespace SmartTraсker.Data.Configurations;
+
+public class TaskConfiguration : IEntityTypeConfiguration<Task>
+{
+    public void Configure(EntityTypeBuilder<Task> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder
+            .HasOne(t => t.Author)
+            .WithMany(u => u.CreatedTasks)
+            .HasForeignKey(t => t.AuthorId);
+        
+        builder
+            .HasOne(t => t.Executor)
+            .WithMany(u => u.AssignedTasks)
+            .HasForeignKey(t => t.ExecutorId);
+
+        builder
+            .HasMany(t => t.Comments)
+            .WithOne(c => c.Task);
+    }
+}
